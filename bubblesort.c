efdfcce6ea565fb01161fd1fb42085dfa3081410
Arathy Rose <arathyrose2000@gmail.com>
2019-10-31 15:04:55 +0530

    Added bubbleSort.c

---
+++ bubblesort.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#define max_size 100005
+int T, a[max_size], n;
+void bubblesort(int a[], int size)
+{
+    for (int i = 1; i < size; i++)
+    {
+        int is_swap = 0;
+        for (int j = 0; j < i; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                int tmp = a[j + 1];
+                a[j + 1] = a[j];
+                a[j] = tmp;
+                is_swap = 1;
+            }
+            if (is_swap == 0)
+                break;
+        }
+    }
+}
+int main()
+{
+    scanf("%d", &T);
+    while (T--)
+    {
+        scanf("%d", &n); // The total number of elements
+        for (int i = 0; i < n; i++)
+            scanf("%d", &a[i]); // Adding the elements to the array
+        bubblesort(a, n);
+        for (int i = 0; i < n; i++)
+            printf("%d ", a[i]);
+        printf("\n");
+    }
+    return 0;
+}
